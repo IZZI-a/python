c48511fb25782d86cf85edf149cf273fcd3de351
prokurator <paleniyvinigret@gmail.com>
2024-11-24 20:47:52 +0300

    Залил проект на 'Github'

---
+++ django/backend/blog/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from blog import views
+
+app_name = "blog"
+
+urlpatterns = [
+    path("index/", views.index, name='index'),
+    path("", views.post_list, name= "post_list"),
+    path("detail/<int:pk>/", views.post_detail, name='post_detail'),
+    path("book2/", views.book, name='user_book'),
+    path("news/", views.news),
+    path("user/", views.book),
+]
